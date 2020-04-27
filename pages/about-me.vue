a5f31ffeaee9e96c6c9ea9045401b0b4eb60d464
Kota Hashihama <kotahashihama@gmail.com>
2020-04-27 23:12:11 +0900

    feat: add about-me and skills page
    Coming-soon page for now

---
+++ pages/about-me.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about section">
+    <div class="container">
+      <div class="section-header text-center my-5">
+        <h2 class="section-title text-uppercase mb-3">
+          <i class="far fa-id-card"></i> About Me
+        </h2>
+        <p class="lead mb-0">
+          I'm Kota Hashihama.<br />
+        </p>
+      </div>
+
+      <div class="row mb-5">
+        <p>Coming soon...</p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/skills.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="skills section">
+    <div class="container">
+      <div class="section-header text-center my-5">
+        <h2 class="section-title text-uppercase mb-3">
+          <i class="far fa-lightbulb"></i> Skills
+        </h2>
+        <p class="lead mb-0">
+          Languages and tools I can command.
+        </p>
+      </div>
+
+      <div class="row">
+        <p>Coming soon...</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.skills {
+  &-stars {
+    font-size: 3rem;
+  }
+}
+</style>
